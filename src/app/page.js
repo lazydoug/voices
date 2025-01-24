57f1caeb89880817de913c71afb0f52e295899df
O. Douglas Idumwonyi <id.doglax@gmail.com>
2025-01-24 15:43:48 +0100

    Initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,93 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className='mt-3 mb-6 w-[640px] max-w-[90vw] mx-auto'>
+      <div className='bg-[url("/img/hero-img.jpg")] bg-center bg-contain rounded-lg h-40 max-h-[22.5vw]'></div>
+      <main>
+        <div className='bg-white border-t-[10px] border-orange-300 text-black mt-3 rounded-lg px-6 py-4 space-y-3'>
+          <h1 className='text-3xl font-bold'>
+            Nomination for Perseverance & Strength Award
+          </h1>
+          <p className='text-[#202124] text-[14px]'>
+            The{' '}
+            <span className='font-semibold italic'>
+              Perseverance & Strength Award
+            </span>{' '}
+            is designed to honour individuals or organisations who have
+            demonstrated exceptional resilience, courage, and determination in
+            the face of adversity. This form allows nominators to share the
+            inspiring stories of nominees, highlighting their journey,
+            achievements, and the positive impact they&apos;ve made on their
+            community, industry, or personal life. By completing this form, you
+            play a vital role in recognising and celebrating the strength and
+            perseverance of unsung heroes who inspire others through their
+            actions and spirit.
+          </p>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          <hr className='-mx-6' />
+
+          <div className='text-[#d93025]'>* Indicates required question</div>
         </div>
+
+        <form action='' className='mt-3'>
+          <fieldset className='rounded-lg bg-white px-6 py-4'>
+            <legend>Nominee Information</legend>
+            <div>
+              <label htmlFor='nomineeName'>
+                Full Name of Nominee (Individual/Organization)
+              </label>
+              <input
+                className='block focus:outline-none'
+                type='text'
+                name='nomineeName'
+                id='nomineeName'
+              />
+            </div>
+
+            <div>
+              <label>Type of Nominee</label>
+              <div className='space-x-2'>
+                <input type='radio' name='nomType' id='indie' />
+                <label htmlFor='indie'>Individual</label>
+              </div>
+
+              <div className='space-x-2'>
+                <input type='radio' name='nomType' id='org' />
+                <label htmlFor='org'>Organization</label>
+              </div>
+            </div>
+
+            <div>
+              <label htmlFor='nomPhone'>Nominee Phone</label>
+              <input
+                className='block focus:outline-none'
+                type='tel'
+                name='nomineePhone'
+                id='nomPhone'
+              />
+            </div>
+
+            <div>
+              <label htmlFor='nomEmail'>Nominee Email</label>
+              <input
+                className='block focus:outline-none focus:pb-[2px] box-border border-b focus:border-b-[3px] focus:border-b-orange-300 transition-colors duration-300'
+                type='email'
+                name='nomineeEmail'
+                id='nomEmail'
+              />
+            </div>
+
+            <div>
+              <label htmlFor='nomAddress'>Nominee Address</label>
+              <input
+                className='block focus:outline-none focus:pb-[2px] box-border border-b focus:border-b-[3px] focus:border-b-orange-300 transition-colors duration-300'
+                type='text'
+                name='nomineeAddress'
+                id='nomAddress'
+              />
+            </div>
+          </fieldset>
+        </form>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
-  );
+  )
 }
